9212e2b56db4ca882d596e0f06b41291d73c5866
sunny142857 <18142505+sunny142857@users.noreply.github.com>
2020-04-08 19:53:57 +0800

    Update(20200408)

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+let EN = ExpantaNum;
 let game;
 function reset() {
   game = {
-    FFCoin: ExpantaNum(0),
+    FFCoin: EN(0),
+	Rabbot: EN(0),
     multi: [
-      ExpantaNum(1),
-      ExpantaNum(1),
-      ExpantaNum(1),
-      ExpantaNum(1),
-      ExpantaNum(1),
-      ExpantaNum(1)
+      EN(1),
+      EN(1),
+      EN(1),
+      EN(1),
+      EN(1),
+      EN(1)
     ],
 	auto: 0,
 	autoMultiplier: [
@@ -21,48 +23,62 @@ function reset() {
 	bulk: 0
   };
   
+  showAll();
+}
+function showAll() {
   showFFCoin();
   showMultipier();
-  showAutoBuyMulti();
+  showAutoBuy();
+  showRabbot();
 }
-let multiplierReq = [
-  ExpantaNum(10),
-  ExpantaNum("1e6"),
-  ExpantaNum("1e11"),
-  ExpantaNum("ee10"),
-  ExpantaNum("ee1000"),
-  ExpantaNum("eee10000")
+const multiplierReq = [
+  EN(10),
+  EN("1e5"),
+  EN("1e9"),
+  EN("1e13"),
+  EN("1e17"),
+  EN("eee10000")
 ];
-let multiplierRatio = [
-  ExpantaNum(1.01),
-  ExpantaNum(1.0001),
-  ExpantaNum(1.000001),
-  ExpantaNum(1.00000001),
-  ExpantaNum(1.0000000001),
-  ExpantaNum("eee10000")
+const multiplierRatio = [
+  EN(1.01),
+  EN(1.0001),
+  EN(1.000001),
+  EN(1.00000001),
+  EN(1.0000000001),
+  EN("eee10000")
 ];
 let bulkBuying = [
-  ExpantaNum(1),
-  ExpantaNum(10),
-  ExpantaNum(100),
-  ExpantaNum(Infinity),
+  EN(1),
+  EN(10),
+  EN(100),
+  EN(Infinity),
 ];
 reset();
+function squareRootSum(n) {
+	let m = EN.floor(n.pow(0.5));
+	return m.times(n.sub((m.pow(2).times(2).add(m.times(3).sub(5))).div(6))).round();
+}
+function ENify(x) {
+  if (typeof x == "number") {
+    return EN(x)
+  } else {
+    let newEN = new EN(0)
+    newEN.array = x.array
+    newEN.sign = x.sign
+    newEN.layer = x.layer
+    return newEN
+  }
+}
 var savePath = "AIGSave.txt"
 function save() {
-  let gameTemp = new Object();
-  gameTemp['FFCoin'] = game['FFCoin'].toString();
-  gameTemp['multi'] = game['multi'].toString();
-  gameTemp['auto'] = game['auto'].toString();
-  gameTemp['autoMultiplier'] = game['autoMultiplier'].toString();
-  gameTemp['bulk'] = game['bulk'].toString();
-  localStorage.setItem(savePath, JSON.stringify(gameTemp));
+  localStorage.setItem(savePath, JSON.stringify(game));
 }
 function loadgame(loadgameTemp) {
-  game['FFCoin'] = ExpantaNum(loadgameTemp['FFCoin']);
-  game['multi'] = (loadgameTemp['multi'].split(',')).map(ExpantaNum);
+  game['FFCoin'] = ENify(loadgameTemp['FFCoin']);
+  game['Rabbot'] = ENify(loadgameTemp['Rabbot']);
+  game['multi'] = (loadgameTemp['multi']).map(ENify);
   game['auto'] = Number(loadgameTemp['auto']);
-  game['autoMultiplier'] = (loadgameTemp['autoMultiplier'].split(',')).map(Number);;
+  game['autoMultiplier'] = (loadgameTemp['autoMultiplier']).map(Number);;
   game['bulk'] = Number(loadgameTemp['bulk']);
 }
 function load() {
@@ -71,14 +87,8 @@ function load() {
   if (loadgameTemp != null) {
     reset();
     loadgame(loadgameTemp);
-    showFFCoin();
-    showMultipier();
-    showAutoBuyMulti();
+	showAll();
   }
-}
-function squareRootSum(n) {
-	let m = ExpantaNum.floor(n.pow(0.5));
-	return m.times(n.sub((m.pow(2).times(2).add(m.times(3).sub(5))).div(6))).round();
 }
 
 
@@ -86,17 +96,23 @@ function gainFFCoin() {
   game.FFCoin = game.FFCoin.add(game.multi[0]);
   showFFCoin();
 }
-function bulkBuyMultiplier(n) {
-  var price = (game.multi[n - 1].pow(0.5).times(multiplierReq[n - 1])).round();
-  if (game.FFCoin.gte(price)) {
-	game.multi[n - 1] = game.multi[n - 1].add(1)
-	game.FFCoin = game.FFCoin.sub(price);
-    showFFCoin();
-    showMultipier();
-  }
+function buyRabbot() {
+	let price = EN("1e21")
+    if (game.FFCoin.gte(price)) {
+		game.Rabbot = game.Rabbot.add(1);
+		game.FFCoin = game.FFCoin.sub(price);
+		showFFCoin();
+		showRabbot();
+    }
+}
+function multiplierPrice(n) {
+	return (game.multi[n - 1].pow(0.5).times(multiplierReq[n - 1])).round();
+}
+function rabmultiplier(n) {
+  
 }
 function multiplier2(n) {
-  let price = (game.multi[n - 1].pow(0.5).times(multiplierReq[n - 1])).round();
+  let price = multiplierPrice(n);
   if (game.FFCoin.gte(price)) {
 	game.multi[n - 1] = game.multi[n - 1].add(game.multi[n]);
 	game.FFCoin = game.FFCoin.sub(price);
@@ -105,100 +121,42 @@ function multiplier2(n) {
   }
 }
 
-function multiplier(n) {
-  if (game.FFCoin.gte(multiplierReq[n - 1])) {
-	  
-    //if (true){
-      let bulk = ExpantaNum.floor(game.FFCoin.div(multiplierReq[n - 1]));
-      game.FFCoin = game.FFCoin.sub(multiplierReq[n - 1].mul(bulk));
-      game.multi[n - 1] = game.multi[n - 1].add(game.multi[n].times(bulk));
-	  if (game.FFCoin.lt(ExpantaNum(0))) {
-		game.FFCoin = ExpantaNum(0);
-	  }
-    //}else{
-    //  game.multi[n-1] = game.multi[n-1].add(game.FFCoin.times(game.multi[n]).div(multiplierReq[n-1]).round())
-    //  game.FFCoin = ExpantaNum(0);
-    //}
-    //document.getElementById("newline").innerHTML += bulk + "<br>";
-    showFFCoin();
-    showMultipier();
-    //not sure if >= is in expantanum
-    // or do we have >= ?
-    // currently you need at least 11 FFCoin to buy this even though it costs 10s
-    //dang
-  }
-}
-/*
-function infinity() {
-  if (game.FFCoin.gt(ExpantaNum("1e308"))) {
-    game.carrots = game.carrots.add(game.FFCoin.log10()).round();
-    game.FFCoin = ExpantaNum(0);
-    showpotatoes();
-    showcarrots();
-  }
-}
-
-function metainfinity() {
-  if (game.carrots.gt(ExpantaNum("ee10"))) {
-    game.beetroots = game.beetroots
-      .add(
-        game.carrots
-          .log10()
-          .log10()
-          .log10()
-      )
-      .round();
-    game.potatoes = ExpantaNum(0);
-    game.carrots = ExpantaNum(0);
-    showpotatoes();
-    showcarrots();
-    showbeetroots();
-  }
-}
-*/
-//I'm going to set up for meta2infinity --moooosey
-/*function meta2infinity() {
-  if (game.beetroots.gt(ExpantaNum("(10^)^50 1"))) {
-    game.potatoStorageFacilities = game.potatoStorageFacilities.add(game.beetroots.slog()).round()
-    game.beetroots = ExpantaNum(0)
-    game.potatoes = ExpantaNum(0)
-    game.carrots = ExpantaNum(0)
-    document.getElementById('newline').innerHTML += "communismception" + '<br>';
-    showpotatoes()
-    showcarrots()
-    showbeetroots()
-    showPSFs()
-  }
-}*/
 function showMultipier(){
-  for (var i = 1; i < 6; i++) {
-    document.getElementById("multi" + i).innerHTML = game.multi[i - 1];
-    document.getElementById("multi" + i + "cost").innerHTML =
-      (game.multi[i - 1].pow(0.5).times(multiplierReq[i - 1])).round();
+  for (var n = 1; n < 6; n++) {
+    document.getElementById("multi" + n).innerHTML = game.multi[n - 1];
+    document.getElementById("multi" + n + "cost").innerHTML = multiplierPrice(n);
   }  
 }
 function showFFCoin() {
   document.getElementById("FFCoin").innerHTML = game.FFCoin;
 }
+function showRabbot() {
+  document.getElementById("Rabbot").innerHTML = game.Rabbot;
+}
 function toggleAutoGainCoin() {
   game.auto = 1 - game.auto;
-  if (game.auto == 1) {
-	document.getElementById("autoGainText").innerHTML = "ON";
-  }else{
-	document.getElementById("autoGainText").innerHTML = "OFF";
-  }
+  showAutoBuy();
 }
 function toggleAutoBuyMulti(n) {
   game.autoMultiplier[n-1] = 1 - game.autoMultiplier[n-1];
-  showAutoBuyMulti();
+  showAutoBuy();
 }
-function showAutoBuyMulti() {
+function showAutoBuy() {
   for (var n = 1; n < 6; n++){
     if (game.autoMultiplier[n-1] == 1) {
 	  document.getElementById("autoBuyMulti"+n).innerHTML = "ON";
+	  document.getElementById("toggleAutoBuyMultiButton"+n).className = ("onButton");
     }else{
 	  document.getElementById("autoBuyMulti"+n).innerHTML = "OFF";
+	  document.getElementById("toggleAutoBuyMultiButton"+n).className = ("offButton");
     }
+  }
+  if (game.auto == 1) {
+	document.getElementById("autoGainText").innerHTML = "ON";
+	  document.getElementById("toggleAutoGainCoinButton").className = ("onButton");
+  }else{
+	document.getElementById("autoGainText").innerHTML = "OFF";
+	  document.getElementById("toggleAutoGainCoinButton").className = ("offButton");
   }
 }
 function toggleBulkBuying() {
